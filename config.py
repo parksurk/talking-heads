7fc13e4ce7d50525c2677ca2c5c390f4dcbb4be3
parksurk <parksurk@gmail.com>
2019-07-09 20:23:09 +0900

    modify config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # VGG_FACE = r'/home/<user>/Documents/NeuralNetworkModels/vgg_face_dag.pth'
-VGG_FACE = r'/home/<user>/models/vgg_face_dag.pth'
+# VGG_FACE = r'/home/<user>/models/vgg_face_dag.pth'
+VGG_FACE = r'/home/antonio/dlc3/models/vgg_face_dag.pth'
 LOG_DIR = r'logs'
 MODELS_DIR = r'models'
 GENERATED_DIR = r'generated_img'
